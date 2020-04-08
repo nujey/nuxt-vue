e48333629f76b7e3fb495b6d8bf52d16c2b6d5e7
张锋 <zhangfeng930101@qq.com>
2020-04-08 15:42:08 +0800

    first

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#38adff', height: '10px' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'iview/dist/styles/iview.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/iview'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Build configuration
+  ** 允许在自动生成的vendor.bundle.js文件中添加模块 减少bundle的体积
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
