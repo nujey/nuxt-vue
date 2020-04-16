cff1a0c831f962f746cc7599c76f8366ffcd6358
张锋 <zhangfeng930101@qq.com>
2020-04-16 17:34:23 +0800

    asyncData

--- pages/study/_uid.vue
+++ pages/study/_uid.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  asyncData({ params }, cb) {
+    console.log(params)
+    // axios.get(`https://my-api/posts/${params.id}`)
+    //   .then(res => {
+    //     cb(null, { title: res.data.title})
+    //   })
+  },
   validate({ params, store }) {
     // return /^\d+$/.test(params.uid)
     // return new Promise((resolve) => setTimeout(() => resolve()))
